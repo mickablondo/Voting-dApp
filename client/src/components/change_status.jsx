d36ce199554109ee190693b345b8754137a71955
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2023-06-25 23:03:56 +0200

    #3 composants selon le profil connecté

---
+++ client/src/components/change_status.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChangeStatus = () => {
+  return (
+    <div>change_status</div>
+  )
+}
+
+export default ChangeStatus
--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Head from "./head";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Col, Container, Row } from 'reactstrap';
@@ -6,10 +6,17 @@ import useEth from "../contexts/EthContext/useEth";
 import VotingStates from "./voting_states";
 import NoticeNoArtifact from "./notices/NoticeNoArtifact";
 import NoticeWrongNetwork from "./notices/NoticeWrongNetwork";
+import ChangeStatus from "./change_status";
+import AddVoter from "./voter/add_voter";
+import ListVoters from "./voter/list_voters";
+import AddProposal from "./proposal/add_proposal";
+import ListProposal from "./proposal/list_proposal";
 
 const Index = () => {
 
   const { state: { artifact, contract, accounts, owner } } = useEth();
+  const [ownerPart, setOwnerPart] = useState();
+  const [voterPart, setVoterPart] = useState();
 
   // Gestion des droits du compte connecté
   useEffect(() => {
@@ -17,14 +24,23 @@ const Index = () => {
           if (contract) {
               if(owner === accounts[0]) {
                   console.log('i am the owner !')
+                  setOwnerPart(<Row>
+                                <ChangeStatus />
+                                <ListVoters />
+                                <AddVoter />
+                              </Row>);
               }
 
               // recherche des voters dans les évènements
               let options = {filter: {value: [],},fromBlock: 0};
-              await contract.events.VoterRegistered(options)
+              contract.events.VoterRegistered(options)
                   .on('data', event => {
                     if(event.returnValues.voterAddress === accounts[0]) {
                       console.log('i am a voter !')
+                      setVoterPart(<Row>
+                                    <ListProposal />
+                                    <AddProposal />
+                                  </Row>);
                     }
                   })
                   .on('changed', changed => console.log(changed))
@@ -36,7 +52,8 @@ const Index = () => {
 
   const body = <Row>
   <Col>
-    CECI EST A GAUCHE
+    {ownerPart}
+    {voterPart}
   </Col>
   <Col>
     <VotingStates />
