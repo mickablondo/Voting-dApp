8432bf5cbfdec333e7066312a5b91c427070c821
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2023-06-26 19:47:43 +0200

    #1 script plus dynamique

--- truffle/scripts/register_voters.js
+++ truffle/scripts/register_voters.js
@@ -1,20 +1,22 @@
-/*
-  Try `truffle exec scripts/get_voting_status.js`, you should `truffle migrate` first.
-
-  Learn more about Truffle external scripts: 
-  https://trufflesuite.com/docs/truffle/getting-started/writing-external-scripts
-*/
+/**
+ * Script to register voters for testing
+ * Try `truffle exec scripts/register_voters.js`, you should `truffle migrate` first.
+ */
 
 const Voting = artifacts.require("Voting");
-const owner = "0x8869242C5BB22a38b48C38786F8c74A975687C0d";
-const voter1 = "0x3236A5A1974700f033e4367BA7361Bd8a5917657";
-const voter2 = "0x2f1E7C34e6c564f4c545520F0ac406c59dB46D8b";
 
 module.exports = async function (callback) {
   const deployed = await Voting.deployed();
 
-  await deployed.addVoter(voter1, {from: owner});
-  await deployed.addVoter(voter2, {from: owner});
+  const _owner = await deployed.owner();
+  const _voter1 = (await web3.eth.getAccounts())[1];
+  const _voter2 = (await web3.eth.getAccounts())[2];
+
+  await deployed.addVoter(_voter1, {from: _owner});
+  console.log('Added voter : ', _voter1);
+
+  await deployed.addVoter(_voter2, {from: _owner});
+  console.log('Added voter : ', _voter2);
 
   callback();
 };
