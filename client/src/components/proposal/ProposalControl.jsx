9e4738a508be6ee2a8a8caa1e1dc26290f2c586c
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2023-06-27 23:02:16 +0200

    #7 check workflow status + reset textarea value

--- client/src/components/proposal/ProposalControl.jsx
+++ client/src/components/proposal/ProposalControl.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useEth from "../../contexts/EthContext/useEth";
 import { Form, Row, Col, Input, Button } from 'reactstrap';
+import EnumWorkflowStatus from '../EnumWorkflowStatus';
 
 const AddProposal = () => {
 
-  // TODO
-  // - si pas la bonne étape, ne pas montrer le bouton
-  // - re-vérif du type d'utilisateur ? (isVoter)
-
   const [proposalInputState, setProposalInputState] = useState("");
+  const [isProposalRunningState, setIsProposalRunningState] = useState(true);
   const { state: {  contract, accounts } } = useEth();
   
   const onProposalInputChange = (value) => {
@@ -19,23 +17,39 @@ const AddProposal = () => {
     e.preventDefault(); 
     try {
       contract.methods.addProposal(proposalInputState).send({from: accounts[0]});
+      setProposalInputState("");
     } catch (err) {
       console.error(err);
     }
   };
 
+  // Vérification du bon état du workflow
+  useEffect(() => {
+    (async function () {
+        if (contract) {
+          const currentStatus = await contract.methods.workflowStatus().call();
+          if(currentStatus == EnumWorkflowStatus.ProposalsRegistrationStarted) {
+            setIsProposalRunningState(false);
+          }
+        }
+    })();
+  }, [contract]);
+
   return (
     <Form onSubmit={onAddProposalSubmit} >
       <Row className="row-cols-lg-auto g-1">
         <Col className="me-auto ">
-          <Input name="proposalText" placeholder="my proposal is ..." id="addProposal" type="textarea" 
+          <Input name="proposalText" placeholder= {
+            !isProposalRunningState ? "my proposal is ..." : "Not the right time to add a proposal !"
+          } id="addProposal" type="textarea" 
             rows="4"
             value={proposalInputState}
             onChange={e => onProposalInputChange(e.target.value)}
+            disabled={isProposalRunningState}
           />
         </Col>
         <Col>
-          <Button>Add proposal</Button>
+          <Button disabled={isProposalRunningState}>Add proposal</Button>
         </Col>
       </Row> 
     </Form>
