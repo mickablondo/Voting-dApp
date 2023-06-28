196e5277b6f97c8142183e9f4652a900f4a9e0a4
Adrien Launay <2265208+alowoa@users.noreply.github.com>
2023-06-28 23:15:25 +0200

    refresh page on account or network change

--- client/src/contexts/EthContext/EthProvider.jsx
+++ client/src/contexts/EthContext/EthProvider.jsx
@@ -6,6 +6,9 @@ import { reducer, actions, initialState } from "./state";
 function EthProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+ 
+
+
   const init = useCallback(
     async artifact => {
       if (artifact) {
@@ -13,25 +16,39 @@ function EthProvider({ children }) {
         const accounts = await web3.eth.requestAccounts();
         const networkID = await web3.eth.net.getId();
         const { abi } = artifact;
+
         let address, contract, owner;
         try {
-          address = artifact.networks[networkID].address;
-          contract = new web3.eth.Contract(abi, address);
+          if (typeof(artifact.networks[networkID]) !== 'undefined') {
+            address = artifact.networks[networkID].address;
+            contract = new web3.eth.Contract(abi, address);
 
-          // Récupération du owner
-          owner = await contract.methods.owner().call();
-          console.log("owner is:"+ owner);
+            // Récupération du owner
+            owner = await contract.methods.owner().call();
+            console.log("owner is:"+ owner);
+          } else {
+            console.log("Bad network - no contract");
+          }
         } catch (err) {
           console.error(err);
+        }
+        // refresh page on account or network change 
+        if (typeof(artifact.networks[networkID]) !== 'undefined') {
+          web3.currentProvider
+          .on('accountsChanged', (accounts) => window.location.reload())
+          .on('networkChanged', (networkId) => window.location.reload());
         }
         dispatch({
           type: actions.init,
           data: { artifact, web3, accounts, networkID, contract, owner }
         });
+      } else {
+        console.log("no artifact");
       }
     }, []);
 
   useEffect(() => {
+    console.log("init");
     const tryInit = async () => {
       try {
         const artifact = require("../../contracts/Voting.json");
@@ -45,6 +62,7 @@ function EthProvider({ children }) {
   }, [init]);
 
   useEffect(() => {
+    console.log("chainChanged, accountsChanged");
     const events = ["chainChanged", "accountsChanged"];
     const handleChange = () => {
       init(state.artifact);
