72bd7fd45edf417f33f621539a61bd6650b15047
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2023-07-01 15:50:44 +0200

    #14 correction lien image pour prod

--- client/src/components/head.jsx
+++ client/src/components/head.jsx
@@ -24,7 +24,7 @@ const Head = ( { isVoter, isOwner }) => {
     <>
       <Navbar className="me-auto" color="light" light>
         <NavbarBrand href="/">
-          <img alt="logo" src="/images/vote-icon.png" className="navbar-icon" />
+          <img alt="logo" src="images/vote-icon.png" className="navbar-icon" />
           Voting
         </NavbarBrand>
         <NavbarText> 
